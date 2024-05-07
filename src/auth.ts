08f03b369bd4f3526a68ed96984413cd8c111744
reverb <janarendvw@gmail.com>
2024-05-07 11:31:17 +0200

    chore: Update NextAuth configuration and add Prisma adapter

--- src/auth.ts
+++ src/auth.ts
@@ -1,11 +1,12 @@
-import NextAuth from "next-auth";
-import Github from "next-auth/providers/github";
-
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Github({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
-});
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import authConfig from "./auth.config"
+ 
+const prisma = new PrismaClient()
+ 
+export const { handlers, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
+})
