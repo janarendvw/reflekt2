b661e0e4fbbfa9865acb64ae09135993d916ee9d
reverb <janarendvw@gmail.com>
2024-05-09 00:17:57 +0200

    refactor: Update dependencies and remove unused code for reflections page

--- src/app/(pages)/reflections/create-new/page.tsx
+++ src/app/(pages)/reflections/create-new/page.tsx
@@ -9,7 +9,7 @@ type Props = {
   index: number
 }
 
-const BigButton = ({model, index}: Props) => {
+function BigButton ({model, index}: Props) {
   return (
     <Link href={`./create-new/${model}`}>
       <motion.div initial={{opacity: 0}} animate={{opacity:1}} transition={{delay: index*0.1}} className="flex aspect-square w-56 items-center justify-center rounded bg-secondary text-lg font-semibold hover:bg-foreground hover:text-background">
@@ -20,7 +20,7 @@ const BigButton = ({model, index}: Props) => {
   )
 }
 
-function Page({}: Props) {
+function Page() {
   const models = Object.values(ReflectionModelType)
 
   return (
