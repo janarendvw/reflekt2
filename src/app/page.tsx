0a52eba31843ea7675e3ce06a5fb553e2f73579c
reverb <janarendvw@gmail.com>
2024-05-13 11:11:48 +0200

    refactor: Update primary color in globals.css and update page title in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,14 @@ import Image from 'next/image'
 import { getTotalActionPointCount } from './server/actions/action-point'
 import MatrixBackgound from './_components/matrix-background'
 import { useRouter } from 'next/navigation'
+import { Book, ChevronRight, Redo, Rocket } from 'lucide-react'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
   const router = useRouter()
+  const [title, setTitle] = useState<string>('REFLECT')
+
+
 
   
   return (
@@ -15,15 +20,15 @@ export default function Home() {
         <MatrixBackgound />
       </div>
       <main className='flex flex-col items-center'>
-        <h1 className='text-6xl font-bold font-mono text-center'>
-        REFLECT / IMPROVE / <span className='text-primary'>PREVAIL</span>
+        <h1 className='text-[5vw] font-bold text-center font-mono tracking-widest leading-none flex flex-col justify-center items-start'>
+        {title} <span>IMPROVE</span> <span className='text-primary'>CONQUER</span>
         </h1>
         <div className="flex items-center gap-4 mt-16">
           <Button variant='outline' size='lg' onClick={() => router.push('/reflections')}>
-           Learn more
+           <Book size={16}  className='mr-2'/> Learn more
           </Button>
           <Button variant='default' size='lg' onClick={() => router.push('/reflections')}>
-            Get started
+           <Rocket size={16} className='mr-2'/> Get started
           </Button>
         </div>
       </main>
